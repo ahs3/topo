3eb9edd7726d35de60a29fcda5acdcca6dd6ecc9
Al Stone <ahs3@ahs3.net>
2020-08-29 20:15:08 -0600

    Clarify the licensing
    Signed-off-by: Al Stone <ahs3@ahs3.net>

--- topo.c
+++ topo.c
@@ -1,8 +1,23 @@
 /*
  * topo : build quick topology graph from acpi tables
  *
- * params:
- * $1 => PPTT to start from
+ *
+ *  Copyright (c) 2020, Al Stone <ahs3@ahs3.net>
+ *
+ *  This file is part of the RISC-V assembler program cal.
+ *
+ *  cal is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation, either version 2 of the License, or (at your
+ *  option) any later version.
+ *
+ *  Cal is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ *  License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with cal.  If not, see <https://www.gnu.org/licenses/>.
  *
  */
 
